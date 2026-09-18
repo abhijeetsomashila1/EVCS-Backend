bec2b7d515eb39e4853d316ae271f243f061adda
Abhijeet Somashila <165672033+abhijeetsomashila1@users.noreply.github.com>
2026-09-18 13:23:03 +0530

    inverted on and off

--- evoff.sh
+++ evoff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Force Relay OFF (Output LOW)
-pinctrl set 17 op dl 2>/dev/null || raspi-gpio set 17 op dl
+# Force Relay OFF (Input High-Z stops current for SSR)
+pinctrl set 17 ip pn 2>/dev/null || raspi-gpio set 17 ip pn
--- evon.sh
+++ evon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Force Relay ON (Input/High-Z)
-pinctrl set 17 ip pn 2>/dev/null || raspi-gpio set 17 ip pn
+# Force Relay ON (Output LOW sinks current for SSR)
+pinctrl set 17 op dl 2>/dev/null || raspi-gpio set 17 op dl
